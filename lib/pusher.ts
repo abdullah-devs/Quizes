d969531d05e3f8e2623404cafacc044847311fd0
Abdullah Abdulkareem <channel.redrex@gmail.com>
2026-03-18 23:05:26 +0300

    Fix public env variables

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -5,15 +5,15 @@ export const pusherServer = new PusherServer({
     appId: process.env.PUSHER_APP_ID!,
     key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
     secret: process.env.PUSHER_SECRET!,
-    host: process.env.NEXT_PUSHER_APP_HOST!,
-    port: process.env.NEXT_PUSHER_APP_PORT,
+    host: process.env.NEXT_PUBLIC_PUSHER_APP_HOST!,
+    port: process.env.NEXT_PUBLIC_PUSHER_APP_PORT,
     useTLS: true,
 });
 
 export const pusherClient = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
-    wsHost: process.env.NEXT_PUSHER_APP_HOST,
-    wsPort: Number(process.env.NEXT_PUSHER_APP_PORT),
-    wssPort: Number(process.env.NEXT_PUSHER_APP_PORT), 
+    wsHost: process.env.NEXT_PUBLIC_PUSHER_APP_HOST,
+    wsPort: Number(process.env.NEXT_PUBLIC_PUSHER_APP_PORT),
+    wssPort: Number(process.env.NEXT_PUBLIC_PUSHER_APP_PORT), 
     forceTLS: true,
     disableStats: true,
     enabledTransports: ['ws', 'wss'],
